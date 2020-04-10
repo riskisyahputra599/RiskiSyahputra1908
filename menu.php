6ca3eb1886ccb611f6699ed82c1a96a74b9bc0b0
riskisyahputra599 <63256415+riskisyahputra599@users.noreply.github.com>
2020-04-11 00:22:26 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE  html PUBLIC "-//W3C//DTD XHTML 1.0 Transiition//EN" "http://tugaswebuinsu.epizy.com/?i=2/DTD/xhtmll-Transiition.dtd">
+<head>
+<meta htpp-equiv="content-type" content= "text/html; charset=utf-8"/>
+<title>modularitas</title>
+</head>
+
+<body>
+<a href ="RISKI SYAHPUTRA.php">RISKI SYAHPUTRA</a> |
+<a href ="ILKOM-3.php">ILKOM-3</a> |
+<a href ="index.php">home</a> |
+<a href ="about.php">about us</a> |
+<a href ="link.php">links</a> |
+<a href ="content.php">content</a> |
+<a href ="new.php">new menu</a> |
+</body>
+</html>
